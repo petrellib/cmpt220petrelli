fac7a503e2c364c6e4ef6b794a12345538b482ad
petrellib <42783449+petrellib@users.noreply.github.com>
2018-09-28 18:57:42 -0400

    Add files via upload

---
+++ Labs/4/driver_lab4_7.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class driver_lab4_7 {
+
+	 static int removeDuplicates(int a[], int n) 
+	    { 
+	        
+	        if (n==0 || n==1) 
+	            return n; 
+	       
+	        int[] temp = new int[n]; 
+	          
+	       
+	        int j = 0; 
+	        for (int i=0; i<n-1; i++) 
+	           
+	            if (a[i] != a[i+1]) 
+	                temp[j++] = a[i]; 
+	          
+	       
+	        temp[j++] = a[n-1];    
+	          
+	     
+	        for (int i=0; i<j; i++) 
+	            a[i] = temp[i]; 
+	       
+	        return j; 
+	    } 
+	 
+	public static void main(String[] args) {
+		
+		int n = 0;
+		
+		Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+            
+        }
+		
+        Arrays.sort(a);
+        
+        n = removeDuplicates(a, n); 
+   
+        
+        for (int i=0; i<n; i++) 
+           System.out.print(a[i]+" "); 
+
+	}
+
+}
